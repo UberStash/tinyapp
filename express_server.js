4fa3b0a6c68944e35eebd7afaa09671cb7fa2dc4
UberStash <expiredserver@gmail.com>
2020-08-03 19:54:16 +0000

    added body parder to the project and also a post listener for express_server.js

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
 
@@ -13,9 +15,18 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Got it ;)')
 });
 
 
@@ -23,6 +34,7 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
   res.render('urls_show', templateVars);
 });
+
 
 
 app.get("/urls.json", (req, res) => {
